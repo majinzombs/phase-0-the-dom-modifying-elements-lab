01089af4b4ef56ebc88ef1fb6b769162168070e4
Jonathan Koban <jon.koban@outlook.com>
2022-04-12 09:59:52 -0400

    commit tree 3ff97c245982d7a695190604fbb56f1573a039f8

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+/* Enter the code to remove the node element here */
+const main = document.querySelector('main#main').remove();
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "YOUR-NAME is the champion!";
+newHeader.className = "victory";
+console.log(newHeader)
